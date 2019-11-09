e433168683088723c8bf870c29a6afd38be36039
Krzysztof Lis <krzysztof.lis@epfl.ch>
2019-11-09 19:04:27 +0100

    cuda10.1 + pytorch1.3.0

--- images/build-cuda-ext.sh
+++ images/build-cuda-ext.sh
@@ -1,7 +1,13 @@
 
-docker build lab-base --build-arg BASE_IMG=nvidia/cuda:10.0-cudnn7-devel-ubuntu18.04 -t ic-registry.epfl.ch/cvlab/lis/lab-base:cuda10-devel 
+docker build lab-base --build-arg BASE_IMG=nvidia/cuda:10.1-cudnn7-devel-ubuntu18.04 -t ic-registry.epfl.ch/cvlab/lis/lab-base:cuda10.1-devel 
 docker build lab-pytorch-cuda-ext -t ic-registry.epfl.ch/cvlab/lis/lab-pytorch-cuda-ext:latest
 docker build lab-pytorch-apex -t ic-registry.epfl.ch/cvlab/lis/lab-pytorch-apex:latest
+docker build lab-python-ml -t ic-registry.epfl.ch/cvlab/lis/lab-python-ml:latest
 
-docker push ic-registry.epfl.ch/cvlab/lis/lab-pytorch-cuda-ext:latest
-docker push ic-registry.epfl.ch/cvlab/lis/lab-pytorch-apex:latest
+# docker push ic-registry.epfl.ch/cvlab/lis/lab-pytorch-cuda-ext:latest
+# docker push ic-registry.epfl.ch/cvlab/lis/lab-pytorch-apex:latest
+# docker push ic-registry.epfl.ch/cvlab/lis/lab-python-ml:latest
+
+docker tag ic-registry.epfl.ch/cvlab/lis/lab-python-ml:latest ic-registry.epfl.ch/cvlab/lis/lab-python-ml:cuda10.1-pytorch1.3.0-tf2.0.0
+
+
