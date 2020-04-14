24e4633e8248e60d6d850fa0c1c2465c1b50814e
Krzysztof Lis <adynathos@gmail.com>
2020-04-14 15:43:08 +0200

    organize build script

--- images/build.sh
+++ images/build.sh
@@ -1,21 +1,36 @@
- 
-# docker build lab-base --build-arg BASE_IMG=ubuntu:disco -t ic-registry.epfl.ch/cvlab/lis/lab-base:cpu 
 
-docker build lab-base --build-arg BASE_IMG=nvidia/cuda:10.0-cudnn7-runtime-ubuntu18.04 -t ic-registry.epfl.ch/cvlab/lis/lab-base:cuda10 
+docker build lab-base \
+	--build-arg BASE_IMG=nvidia/cuda:10.1-cudnn7-devel-ubuntu18.04 \
+	-t ic-registry.epfl.ch/cvlab/lis/lab-base:cuda10.1-devel 
 
-docker build lab-base --build-arg BASE_IMG=nvidia/cuda:10.0-cudnn7-devel-ubuntu18.04 -t ic-registry.epfl.ch/cvlab/lis/lab-base:cuda10-devel 
+docker build lab-pytorch-cuda-ext \
+	--build-arg BASE_IMG=ic-registry.epfl.ch/cvlab/lis/lab-base:cuda10.1-devel \
+	-t ic-registry.epfl.ch/cvlab/lis/lab-pytorch-cuda-ext:dev
 
-docker build lab-python-ml -t ic-registry.epfl.ch/cvlab/lis/lab-python-ml:cuda10
-docker build lab-python-extra -t ic-registry.epfl.ch/cvlab/lis/lab-python-extra-example:cuda10
+docker build lab-pytorch-apex \
+	--build-arg BASE_IMG=ic-registry.epfl.ch/cvlab/lis/lab-pytorch-cuda-ext:dev \
+	-t ic-registry.epfl.ch/cvlab/lis/lab-pytorch-apex:dev
 
-# docker push ic-registry.epfl.ch/cvlab/lis/lab-base:cpu 
+docker build lab-python-ml \
+	--build-arg BASE_IMG=ic-registry.epfl.ch/cvlab/lis/lab-pytorch-apex:dev \
+	-t ic-registry.epfl.ch/cvlab/lis/lab-python-ml:dev
 
-docker push ic-registry.epfl.ch/cvlab/lis/lab-base:cuda10
 
-docker tag ic-registry.epfl.ch/cvlab/lis/lab-python-ml:cuda10  ic-registry.epfl.ch/cvlab/lis/lab-python-ml:latest
-docker push ic-registry.epfl.ch/cvlab/lis/lab-python-ml:cuda10
-docker push ic-registry.epfl.ch/cvlab/lis/lab-python-ml:latest
+# wget https://github.com/colmap/colmap/archive/3.6-dev.3.zip -O lab-colmap/3.6-dev.3.zip
 
-docker tag ic-registry.epfl.ch/cvlab/lis/lab-python-extra-example:cuda10  ic-registry.epfl.ch/cvlab/lis/lab-python-extra-example:latest
-docker push ic-registry.epfl.ch/cvlab/lis/lab-python-extra-example:cuda10
-docker push ic-registry.epfl.ch/cvlab/lis/lab-python-extra-example:latest
+docker build lab-colmap \
+	--build-arg BASE_IMG=ic-registry.epfl.ch/cvlab/lis/lab-pytorch-apex:dev \
+	-t ic-registry.epfl.ch/cvlab/lis/lab-pytorch-apex-colmap:dev
+
+
+docker push ic-registry.epfl.ch/cvlab/lis/lab-base:cuda10.1-devel
+docker push ic-registry.epfl.ch/cvlab/lis/lab-pytorch-apex:dev
+docker push ic-registry.epfl.ch/cvlab/lis/lab-python-ml:dev
+
+# docker push ic-registry.epfl.ch/cvlab/lis/lab-pytorch-cuda-ext:cuda10.1-pytorch1.4.0
+# docker push ic-registry.epfl.ch/cvlab/lis/lab-pytorch-cuda-ext:latest 
+# docker push ic-registry.epfl.ch/cvlab/lis/lab-pytorch-apex:latest
+# docker push ic-registry.epfl.ch/cvlab/lis/lab-pytorch-apex:cuda10.1-pytorch1.4.0
+# docker push ic-registry.epfl.ch/cvlab/lis/lab-python-ml:latest 
+# docker push ic-registry.epfl.ch/cvlab/lis/lab-python-ml:cuda10.1-pytorch1.4.0-tf2.1.0
+
