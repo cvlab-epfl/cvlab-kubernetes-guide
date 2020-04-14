ce1c2237554efc5c4cb86e70a900ad875c79e6e2
Krzysztof Lis <adynathos@gmail.com>
2020-04-14 15:41:50 +0200

    sleeper in base

--- images/build-cuda-ext.sh
+++ images/build-cuda-ext.sh
@@ -5,31 +5,27 @@ docker build lab-base \
 
 docker build lab-pytorch-cuda-ext \
 	--build-arg BASE_IMG=ic-registry.epfl.ch/cvlab/lis/lab-base:cuda10.1-devel \
-	-t ic-registry.epfl.ch/cvlab/lis/lab-pytorch-cuda-ext:latest
+	-t ic-registry.epfl.ch/cvlab/lis/lab-pytorch-cuda-ext:dev
 
 docker build lab-pytorch-apex \
-	--build-arg BASE_IMG=ic-registry.epfl.ch/cvlab/lis/lab-pytorch-cuda-ext:latest \
-	-t ic-registry.epfl.ch/cvlab/lis/lab-pytorch-apex:latest
+	--build-arg BASE_IMG=ic-registry.epfl.ch/cvlab/lis/lab-pytorch-cuda-ext:dev \
+	-t ic-registry.epfl.ch/cvlab/lis/lab-pytorch-apex:dev
 
 docker build lab-python-ml \
-	--build-arg BASE_IMG=ic-registry.epfl.ch/cvlab/lis/lab-pytorch-apex:latest \
-	-t ic-registry.epfl.ch/cvlab/lis/lab-python-ml:latest
+	--build-arg BASE_IMG=ic-registry.epfl.ch/cvlab/lis/lab-pytorch-apex:dev \
+	-t ic-registry.epfl.ch/cvlab/lis/lab-python-ml:dev
 
 
-docker build lab-sleeper-addon \
-	--build-arg BASE_IMG=ic-registry.epfl.ch/cvlab/lis/lab-pytorch-apex:latest \
-	-t ic-registry.epfl.ch/cvlab/lis/lab-pytorch-apex:sleeper
+# wget https://github.com/colmap/colmap/archive/3.6-dev.3.zip -O lab-colmap/3.6-dev.3.zip
 
-# docker push ic-registry.epfl.ch/cvlab/lis/lab-pytorch-cuda-ext:latest
-# docker push ic-registry.epfl.ch/cvlab/lis/lab-pytorch-apex:latest
-# docker push ic-registry.epfl.ch/cvlab/lis/lab-python-ml:latest
+docker build lab-colmap \
+	--build-arg BASE_IMG=ic-registry.epfl.ch/cvlab/lis/lab-pytorch-apex:dev \
+	-t ic-registry.epfl.ch/cvlab/lis/lab-pytorch-apex-colmap:dev
 
-# docker tag ic-registry.epfl.ch/cvlab/lis/lab-pytorch-cuda-ext:latest ic-registry.epfl.ch/cvlab/lis/lab-pytorch-cuda-ext:cuda10.1-pytorch1.4.0
-# docker tag ic-registry.epfl.ch/cvlab/lis/lab-pytorch-apex:latest ic-registry.epfl.ch/cvlab/lis/lab-pytorch-apex:cuda10.1-pytorch1.4.0
-# docker tag ic-registry.epfl.ch/cvlab/lis/lab-python-ml:latest ic-registry.epfl.ch/cvlab/lis/lab-python-ml:cuda10.1-pytorch1.4.0-tf2.1.0
 
-docker tag ic-registry.epfl.ch/cvlab/lis/lab-pytorch-apex:latest ic-registry.epfl.ch/cvlab/lis/lab-pytorch-apex:sleeper
-docker push ic-registry.epfl.ch/cvlab/lis/lab-pytorch-apex:sleeper
+docker push ic-registry.epfl.ch/cvlab/lis/lab-base:cuda10.1-devel
+docker push ic-registry.epfl.ch/cvlab/lis/lab-pytorch-apex:dev
+docker push ic-registry.epfl.ch/cvlab/lis/lab-python-ml:dev
 
 # docker push ic-registry.epfl.ch/cvlab/lis/lab-pytorch-cuda-ext:cuda10.1-pytorch1.4.0
 # docker push ic-registry.epfl.ch/cvlab/lis/lab-pytorch-cuda-ext:latest 
